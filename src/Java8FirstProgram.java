2f2fdff30599b824b2ad5a4a8c2accbe823dc13d
sanjit21mahato <sanjit21mahato@gmail.com>
2025-01-25 13:10:31 +0530

    dev

--- src/Java8FirstProgram.java
+++ src/Java8FirstProgram.java
@@ -6,10 +6,11 @@ public class Java8FirstProgram {
         Java8FirstProgram j8 = new Java8FirstProgram();
         j8.test1();
 
-
     }
 
     private void test1() {
         System.out.println("testing");
+        System.out.println("in dev");
+
     }
 }
