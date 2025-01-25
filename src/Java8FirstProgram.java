6b33c3523201e06e710a0bb6dc89cde45763c1a0
sanjit21mahato <sanjit21mahato@gmail.com>
2025-01-25 13:06:46 +0530

    test

---
+++ src/Java8FirstProgram.java
@@ -0,0 +1,15 @@
+public class Java8FirstProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("java8");
+        Java8FirstProgram j8 = new Java8FirstProgram();
+        j8.test1();
+
+
+    }
+
+    private void test1() {
+        System.out.println("testing");
+    }
+}
